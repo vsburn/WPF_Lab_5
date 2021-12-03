3a33a18041612651729276fd14ffdbbf7feb6037
Fcss Void <fcss.void@gmail.com>
2021-12-03 22:22:06 +0300

    Добавьте файлы проекта.

---
+++ WindowApp_lab/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowApp_lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Top = Properties.Settings.Default.Position.Top;
+            this.Left = Properties.Settings.Default.Position.Left;
+            this.Height = Properties.Settings.Default.Position.Height;
+            this.Width = Properties.Settings.Default.Position.Width;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Position = this.RestoreBounds;
+            Properties.Settings.Default.Save();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+                textBox.FontSize = fontSize;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Underline)
+                textBox.TextDecorations = null;
+            else
+                textBox.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void btn_black_Checked(object sender, RoutedEventArgs e)
+        {
+            if (btn_blue != null)
+            {
+                if (!btn_blue.IsPressed && textBox != null)
+                    textBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void btn_blue_Checked(object sender, RoutedEventArgs e)
+        {
+            if (btn_black != null)
+            {
+                if (!btn_black.IsPressed && textBox != null)
+                    textBox.Foreground = new SolidColorBrush(Colors.Blue);
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
